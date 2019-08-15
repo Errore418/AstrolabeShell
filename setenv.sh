11151a9573a3b0b8dca51b099d945bfeda5ebef0
Claudio Nave <claudionave97@gmail.com>
2019-08-15 18:59:44 +0200

    Fixata formattazione setenv di prova

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
-#CATALINA_OPTS="$CATALINA_OPTS @@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
-#CATALINA_OPTS="$CATALINA_OPTS @@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
-#CATALINA_OPTS="$CATALINA_OPTS @@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
+#CATALINA_OPTS="$CATALINA_OPTS 1@@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
+#CATALINA_OPTS="$CATALINA_OPTS 2@@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
+#CATALINA_OPTS="$CATALINA_OPTS 3@@.@@@@@@@@@@@@@.@@@@.liquibase.@@@@@@.@@@=true"
