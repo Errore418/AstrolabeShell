d5c03be18b2ee6689ad0d867eca8ab30bda0fc21
Claudio Nave <claudionave97@gmail.com>
2019-08-15 18:48:11 +0200

    Aggiunta scelta guidata

--- toggle-properties.sh
+++ toggle-properties.sh
@@ -2,14 +2,16 @@
 file=setenv.sh
 propertiesToSearch=liquibase
 commentCharacter=#
+enableArgument=enable
+disableArgument=disable
 
 function loop_over_properties {
 	local IFS=$'\n'
 	for line in $(grep $propertiesToSearch $file)
 	do
-		if [[ $1 = "enable" ]]; then
+		if [[ $1 = $enableArgument ]]; then
 			enable_properties $line
-		elif [[ $1 = "disable" ]]; then
+		elif [[ $1 = $disableArgument ]]; then
 			disable_properties $line
 		fi
 	done
@@ -34,6 +36,20 @@ function change_properties {
 	echo "$3 $1"
 }
 
-loop_over_properties $1
-
+if [ "$#" = "0" ]; then
+	echo "This routine is for enable or disable lines that contains $propertiesToSearch in file $file"
+	echo "Select an option:"
+	echo "0 - Enable lines"
+	echo "1 - Disable lines"
+	echo -n "Your choice: "
+	read -n1 choice
+	echo -e -n "\n"
+	if [[ $choice = "0" ]]; then
+		loop_over_properties $enableArgument
+	elif [[ $choice = "1" ]]; then
+		loop_over_properties $disableArgument
+	fi
+else
+	loop_over_properties $1	
+fi
 exit 0
