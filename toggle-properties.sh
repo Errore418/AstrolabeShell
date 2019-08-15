f6f9204f41f3f0c7f374016158687dd6ccd727ec
Claudio Nave <claudionave97@gmail.com>
2019-08-15 18:46:55 +0200

    Implementata scelta per attivazione o disattivazione linee

--- toggle-properties.sh
+++ toggle-properties.sh
@@ -3,16 +3,37 @@ file=setenv.sh
 propertiesToSearch=liquibase
 commentCharacter=#
 
+function loop_over_properties {
+	local IFS=$'\n'
+	for line in $(grep $propertiesToSearch $file)
+	do
+		if [[ $1 = "enable" ]]; then
+			enable_properties $line
+		elif [[ $1 = "disable" ]]; then
+			disable_properties $line
+		fi
+	done
+}
 
-IFS=$'\n'
-for line in $(grep $propertiesToSearch $file)
-do
-	if [[ $line =~ $commentCharacter ]]; then
+function enable_properties {
+	if [[ $1 =~ $commentCharacter ]]; then
 		stringToInsert="${line//$commentCharacter}"
-		echo "Enabled line $line"
-	else
-		stringToInsert="${commentCharacter}${line}"
-		echo "Disabled line $line"
+		change_properties $line $stringToInsert "Enabled line"
 	fi
-	sed -i "s/${line}/${stringToInsert}/g" "$file"
-done
+}
+
+function disable_properties {
+	if [[ ! $1 =~ $commentCharacter ]]; then
+		stringToInsert="${commentCharacter}${line}"
+		change_properties $line $stringToInsert "Disabled line"
+	fi
+}
+
+function change_properties {
+	sed -i "s/${1}/${2}/g" "$file"
+	echo "$3 $1"
+}
+
+loop_over_properties $1
+
+exit 0
