250715085f9f01f65ce0941527c1c962f7a3c5fc
Claudio Nave <claudionave97@gmail.com>
2019-08-22 19:28:44 +0200

    Gestiti spazi nei nomi dei file

--- context.sh
+++ context.sh
@@ -10,21 +10,21 @@ aliasProperty=ALIAS
 fileProperty=FILE
 propertySeparator=:
 
-contextFiles=($(ls $contextDir)) #UNSTABLE!!!
+mapfile -d '' contextFiles < <(find $contextDir -type f -printf "%f\0")
 
 function printContextFiles {
 	for i in "${!contextFiles[@]}"; do
 		regex="(\\${temporarySuffix}|\\${archivedSuffix})"
-		if ! [[ ${contextFiles[$i]} = $currentContext || ${contextFiles[$i]} =~ $regex ]]; then
-			setProperty ${contextFiles[$i]} $fileProperty ${contextFiles[$i]}
+		if ! [[ "${contextFiles[$i]}" = $currentContext || "${contextFiles[$i]}" =~ $regex ]]; then
+			setProperty "${contextFiles[$i]}" $fileProperty "${contextFiles[$i]}"
 		fi
-		fileWithAlias=$(formatFileWithAlias ${contextFiles[$i]})
+		fileWithAlias=$(formatFileWithAlias "${contextFiles[$i]}")
 		echo "$i - $fileWithAlias"
 	done
 }
 
 function formatFileWithAlias {
-	alias=$(findProperty ${1} $aliasProperty)
+	alias=$(findProperty "${1}" $aliasProperty)
 	result="$1"
 	if [[ ! -z $alias ]]; then
 		result="$result ($alias)"
@@ -34,23 +34,23 @@ function formatFileWithAlias {
 
 function findProperty {
 	fileToScan="${contextDir}/${1}"
-	grep -oP "(?<=${openComment}${2}${propertySeparator}).+(?=${closeComment})" $fileToScan
+	grep -oP "(?<=${openComment}${2}${propertySeparator}).+(?=${closeComment})" "$fileToScan"
 }
 
 function dropProperty {
 	targetFile="${contextDir}/${1}"
-	sed -i "/${openComment}${2}${propertySeparator}/d" $targetFile
+	sed -i "/${openComment}${2}${propertySeparator}.*${closeComment}/d" "$targetFile"
 }
 
 function setProperty {
-	dropProperty ${1} ${2}
-	echo "${openComment}${2}${propertySeparator}${3}${closeComment}" >> $targetFile
+	dropProperty "${1}" ${2}
+	echo "${openComment}${2}${propertySeparator}${3}${closeComment}" >> "$targetFile"
 }
 
 function acquireAlias {	
 	echo -n "Insert an alias: "
 	read newAlias
-	setProperty $1 $aliasProperty "$newAlias"
+	setProperty "$1" $aliasProperty "$newAlias"
 	echo "Alias ${newAlias} correctly registered for file ${1}"
 }
 
@@ -60,31 +60,31 @@ function swapFiles {
 	if [[ -z $archivedFile ]]; then
 		archivedFile="${currentContext}${archivedSuffix}"
 	fi
-	renameFile $currentContext $tempFile
-	renameFile $1 $currentContext
-	renameFile $tempFile $archivedFile
+	renameFile "$currentContext" "$tempFile"
+	renameFile "$1" "$currentContext"
+	renameFile "$tempFile" "$archivedFile"
 	echo "Swapping completed"
 }
 
 function renameFile {
 	oldFile="${contextDir}/${1}"
 	newFile="${contextDir}/${2}"
-	mv $oldFile $newFile
+	mv "$oldFile" "$newFile"
 	echo "$1 renamed in $2"
 }
 
 function fileSelected {
-	echo "The file $(formatFileWithAlias $1) has been selected"
+	echo "The file $(formatFileWithAlias "$1") has been selected"
 	echo "Select an option:"
 	echo "0 - Swap with ${currentContext}"
 	echo "1 - Set alias"
 	echo -n "Your choice: "
 	read -n1 choice
 	echo -e -n "\n"
-	if [[ $choice = "0" && $1 != $currentContext ]]; then
-		swapFiles $1
+	if [[ $choice = "0" && "$1" != $currentContext ]]; then
+		swapFiles "$1"
 	elif [[ $choice = "1" ]]; then
-		acquireAlias $1
+		acquireAlias "$1"
 	fi
 }
 
