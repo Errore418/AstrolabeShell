865a01755ea7139b765e824738b5993c6db17eeb
Claudio Nave <claudionave97@gmail.com>
2019-08-22 22:23:01 +0200

    Inserita context routine nel file principale

--- tomcat-support.sh
+++ tomcat-support.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+
 source variables.sh
 
-toggleRoutineArgument=toggle
-enableArgument=enable
-disableArgument=disable
+### TOGGLE ROUTINE ###
 
 function loop_over_lines {
 	local IFS=$'\n'
-	for line in $(grep $stringToSearch $file); do
+	for line in $(grep $stringToSearch $toggleFile); do
 		if [[ $1 = $enableArgument ]]; then
 			enable_line $line
 		elif [[ $1 = $disableArgument ]]; then
@@ -31,7 +30,7 @@ function disable_line {
 }
 
 function change_line {
-	sed -i "s/${1}/${2}/g" "$file"
+	sed -i "s/${1}/${2}/g" "$toggleFile"
 	echo "$3 $1"
 }
 
@@ -40,7 +39,7 @@ function toggle_routine {
 		echo "======================"
 		echo "=== Toggle routine ==="
 		echo "======================"
-		echo "This routine is for enabling or disabling lines that contains $stringToSearch in file $file"
+		echo "This routine is for enabling or disabling lines that contains $stringToSearch in file $toggleFile"
 		echo "Select an option:"
 		echo "0 - Enable lines"
 		echo "1 - Disable lines"
@@ -57,19 +56,129 @@ function toggle_routine {
 	fi
 }
 
+### CONTEXT ROUTINE ###
+
+function print_context_files {
+	for i in "${!contextFiles[@]}"; do
+		regex="(\\${temporarySuffix}|\\${archivedSuffix})"
+		if ! [[ "${contextFiles[$i]}" = $currentContext || "${contextFiles[$i]}" =~ $regex ]]; then
+			set_property "${contextFiles[$i]}" $fileProperty "${contextFiles[$i]}"
+		fi
+		fileWithAlias=$(format_file_with_alias "${contextFiles[$i]}")
+		if [[ "${contextFiles[$i]}" = $currentContext ]]; then
+			oldName=$(find_property "${contextFiles[$i]}" $fileProperty)
+			fileWithAlias=$(append_if_not_null "${fileWithAlias}" "${oldName}")
+		fi
+		echo "$i - $fileWithAlias"
+	done
+}
+
+function format_file_with_alias {
+	alias=$(find_property "${1}" $aliasProperty)
+	append_if_not_null "${1}" "$alias"
+}
+
+function append_if_not_null {
+	result="$1"
+	if [[ ! -z $2 ]]; then
+		result="$result ($2)"
+	fi
+	echo $result
+}
+
+function find_property {
+	fileToScan="${contextDir}/${1}"
+	grep -oP "(?<=${openComment}${2}${propertySeparator}).+(?=${closeComment})" "$fileToScan"
+}
+
+function drop_property {
+	targetFile="${contextDir}/${1}"
+	sed -i "/${openComment}${2}${propertySeparator}.*${closeComment}/d" "$targetFile"
+}
+
+function set_property {
+	drop_property "${1}" ${2}
+	echo "${openComment}${2}${propertySeparator}${3}${closeComment}" >> "$targetFile"
+}
+
+function acquire_alias {	
+	echo -n "Insert an alias: "
+	read newAlias
+	set_property "$1" $aliasProperty "$newAlias"
+	echo "Alias ${newAlias} correctly registered for file ${1}"
+}
+
+function swap_files {
+	tempFile="${currentContext}${temporarySuffix}"
+	archivedFile=$(find_property $currentContext $fileProperty)
+	if [[ -z $archivedFile ]]; then
+		archivedFile="${currentContext}${archivedSuffix}"
+	fi
+	rename_file "$currentContext" "$tempFile"
+	rename_file "$1" "$currentContext"
+	rename_file "$tempFile" "$archivedFile"
+	echo "Swapping completed"
+}
+
+function rename_file {
+	oldFile="${contextDir}/${1}"
+	newFile="${contextDir}/${2}"
+	mv "$oldFile" "$newFile"
+	echo "$1 renamed in $2"
+}
+
+function file_selected {
+	echo "The file $(format_file_with_alias "$1") has been selected"
+	echo "Select an option:"
+	echo "0 - Swap with ${currentContext}"
+	echo "1 - Set alias"
+	echo -n "Your choice: "
+	read -n1 choice
+	echo -e -n "\n"
+	if [[ $choice = "0" && "$1" != $currentContext ]]; then
+		swap_files "$1"
+	elif [[ $choice = "1" ]]; then
+		acquire_alias "$1"
+	fi
+}
+
+function context_routine {
+	mapfile -d '' contextFiles < <(find $contextDir -maxdepth 1 -type f -printf "%f\0")
+	if [[ "$#" = "0" ]]; then
+		echo "======================="
+		echo "=== Context routine ==="
+		echo "======================="
+		echo "This routine is for swapping context files in $contextDir"
+		echo "Select a file:"
+		print_context_files
+		echo -n "Your choice: "
+		read choice
+		if [[ $choice =~ ^[0-9]+$ ]] && [[ "$choice" -ge 0 || "$choice" -le "${#contextFiles[@]}" ]]; then
+			file_selected "${contextFiles[$choice]}"
+		fi
+	fi
+}
+
+### WELCOME ROUTINE ###
+
 if [[ "$#" = "0" ]]; then
 	echo "================================"
 	echo "=== Welcome in TomcatSupport ==="
 	echo "================================"
 	echo "Select an option:"
 	echo "0 - Enable or disable lines"
+	echo "1 - Swap context files"
 	echo -n "Your choice: "
 	read -n1 choice
 	echo -e -n "\n"
 	if [[ $choice = "0" ]]; then
 		toggle_routine
+	elif [[ $choice = "1" ]]; then
+		context_routine
 	fi
 elif [[ $1 = $toggleRoutineArgument ]]; then
 	toggle_routine $2
+elif [[ $1 = $contextRoutineArgument ]]; then
+	context_routine
 fi
 exit 0
--- variables.sh
+++ variables.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-#file=PATH TO FILE
+### TOGGLE VARIABLE ###
+
+#toggleFile=PATH TO TOGGLE FILE
 
 stringToSearch=liquibase
 commentCharacter=#
 
-if [[ -z "$file" ]]; then
-	echo "Variable \"file\" is not set"
-	exit 1
-fi
+### CONTEXT VARIABLE ###
+
+#contextDir=PATH TO CONTEXT DIRECTORY
+#currentContext=NAME OF CURRENT CONTEXT
+
+archivedSuffix=.archived
+temporarySuffix=.temp
+openComment="<!--"
+closeComment="-->"
+aliasProperty=ALIAS
+fileProperty=FILE
+propertySeparator=:
+
+### SYSTEM VARIABLE ###
+
+toggleRoutineArgument=toggle
+contextRoutineArgument=context
+enableArgument=enable
+disableArgument=disable
+
+function checkVariable {	
+	if [[ -z "${1}" ]]; then
+		echo "Variable \"${2}\" is not set"
+		exit 1
+	fi
+}
+
+checkVariable "${toggleFile}" "toggleFile"
+checkVariable "${contextDir}" "contextDir"
+checkVariable "${currentContext}" "currentContext"
