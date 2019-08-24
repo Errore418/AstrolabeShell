915870a50eb7d5677279dc1dcb9178aeec0cc1d1
Claudio Nave <claudionave97@gmail.com>
2019-08-25 01:20:20 +0200

    Rispettate direttive licenza gpl

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 name=tomcat-support
 version=1.0
 
-tar -czvf "${name}_${version}.tar.gz" "${name}.sh" variables.sh README.md
+tar -czvf "${name}_${version}.tar.gz" "${name}.sh" variables.sh README.md LICENSE
